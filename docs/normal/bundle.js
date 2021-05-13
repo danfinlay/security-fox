69ed0ba6d36170f429c5b3edacd8e1a82bfed36b
Dan Finlay <dan@danfinlay.com>
2021-05-13 16:13:00 -0700

    Rebuild

--- docs/normal/bundle.js
+++ docs/normal/bundle.js
@@ -1998,9 +1998,11 @@ function createLogoViewer (container, renderScene, {
     shouldRender = true
   }
   function setFollowMouse (state) {
+    // eslint-disable-next-line no-param-reassign
     followMouse = state
   }
   function setFollowMotion (state) {
+    // eslint-disable-next-line no-param-reassign
     followMotion = state
   }
 
